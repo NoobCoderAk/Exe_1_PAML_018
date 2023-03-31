a247f8c2765f2cbaf990e228c0346bf4f74fbdc4
NoobCoderAk <abidakbar0001@gmail.com>
2023-03-31 19:17:46 +0700

    memperbaiki bug pixel layout yang overflow

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -16,120 +16,127 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        width: double.infinity, height: double.infinity,
         child: SingleChildScrollView(
           child: Column(
             children: <Widget> [
-            Container(
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.only(top: 60, bottom: 5),
-              child: Image.asset(
-                'assets/image/pokemon_logo.png',
-                height: 150,
-              ),
-            ),
-            Container( //apply margin and padding using Container Widget.
-              padding: const EdgeInsets.all(5), //You can use EdgeInsets like above
-              margin: const EdgeInsets.only(top: 5),
-              child: const Text(
-                "Let's get started.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 34.0, 
-                  fontWeight: FontWeight.bold,
-                  color: Colors.yellow,
+              Container(
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.only(top: 60, bottom: 5),
+                child: Image.asset(
+                  'assets/image/pokemon_logo.png',
+                  height: 150,
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(5), //You can use EdgeInsets like above
-              margin: const EdgeInsets.only(top: 5, bottom: 40),
-              child: const Text(
-                "There's No Sense In Going Ou Of\nYour Wy To Get Somebody To Like You",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 155, 154, 154),
-                ),
-              ),
-            ),
 
-            //form username
-            Container(
-              padding: EdgeInsets.all(2),
-              margin: EdgeInsets.only(bottom: 10, left: 20,right: 20),
-              child: Column(
-                children: [
-                  TextFormField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter Your name",
-                      labelText: "Name",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0)
-                      )
-                    ),
-                    validator: (_username) {
-                      if (_username == null || _username.isEmpty){
-                       return 'Please enter your name';
-                      }
-                      return null;
-                    },
-                    onSaved: (_username){
-                      username = _username!;
-                    },
+              //text 1
+              Container( //apply margin and padding using Container Widget.
+                padding: const EdgeInsets.all(5), //You can use EdgeInsets like above
+                margin: const EdgeInsets.only(top: 5),
+                child: const Text(
+                 "Let's get started.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 34.0, 
+                    fontWeight: FontWeight.bold,
+                    color: Colors.yellow,
                   ),
-                ],
-              ),
-            ),
-
-            //form pssword
-            Container(
-              padding: EdgeInsets.all(2),
-              margin: EdgeInsets.only(left: 20,right: 20),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter Your Password",
-                      labelText: "Password",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0)
-                      )
-                    ),
-                    validator: (_password) {
-                      if (_password == null || _password.isEmpty){
-                       return 'Please enter your password';
-                      }
-                      return null;
-                    },
-                    onSaved: (_password){
-                      password = _password!;
-                    },
-                  )
-                ],
-              ),
-            ),
-            Container(
-              child: Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: (){
-                        // if (formKey.currentState!.validate()){
-                        //   formKey.currentState!.save();
-                        //   Navigator.push(
-                        //     context,
-                        //     MaterialPageRoute(
-                        //       builder: (context) => StartPage(),
-                        //     )
-                        //   );
-                        // }
-                      }, child: const Text('Login')
-                    ),
-                  ]
                 ),
-            ),
-          ],
+              ),
+
+              //text 2
+              Container(
+                padding: const EdgeInsets.all(5), //You can use EdgeInsets like above
+                margin: const EdgeInsets.only(top: 5, bottom: 40),
+                child: const Text(
+                  "There's No Sense In Going Ou Of\nYour Wy To Get Somebody To Like You",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 155, 154, 154),
+                  ),
+                ),
+              ),
+
+              //form username
+              Container(
+                padding: EdgeInsets.all(2),
+                margin: EdgeInsets.only(bottom: 10, left: 20,right: 20),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter Your name",
+                        labelText: "Name",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0)
+                        )
+                      ),
+                      validator: (_username) {
+                        if (_username == null || _username.isEmpty){
+                         return 'Please enter your name';
+                        }
+                        return null;
+                      },
+                      onSaved: (_username){
+                        username = _username!;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+
+              //form pssword
+              Container(
+                padding: EdgeInsets.all(2),
+                margin: EdgeInsets.only(left: 20,right: 20),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter Your Password",
+                        labelText: "Password",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0)
+                        )
+                      ),
+                      validator: (_password) {
+                        if (_password == null || _password.isEmpty){
+                         return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                      onSaved: (_password){
+                        password = _password!;
+                      },
+                    )
+                  ],
+                ),
+              ),
+
+              //button
+              Container(
+                child: Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: (){
+                          // if (formKey.currentState!.validate()){
+                          //   formKey.currentState!.save();
+                          //   Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //       builder: (context) => StartPage(),
+                          //     )
+                          //   );
+                          // }
+                        }, child: const Text('Login')
+                      ),
+                    ]
+                  ),
+              ),
+            ],
           ),
         ) 
       ),
